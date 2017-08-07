11dfc78f79920b1e4909dd2e4ac0477016b73f96
asunwei <18655519621@163.com>
2017-08-08 00:01:05 +0800

    服务-2

--- myangular2/src/app/app.component.ts
+++ myangular2/src/app/app.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     title = 'Tour of Heroes';
     heroes: Hero[];
     selectedHero: Hero;
-
+    
     onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
     
+  constructor(private heroService:HeroService){}
+
+  getHeroes():void{
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit():void{
+    this.getHeroes();
+  }
+  
 }
 
 
