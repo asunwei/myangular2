8e4cb82c9777a50f0050eb8fac5f086b1a00fe37
asunwei <993525033@qq.com>
2017-08-09 19:46:31 +0800

    路由－２

--- myangular2/src/app/app.module.ts
+++ myangular2/src/app/app.module.ts
@@ -7,15 +7,31 @@ import { HelloDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
 import {RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent,HelloDetailComponent],
+  declarations: [
+    AppComponent,
+    HelloDetailComponent,
+    DashboardComponent,
+    HeroesComponent
+  ],
   imports: [BrowserModule,FormsModule,
   RouterModule.forRoot([
     {
       path:'heroes',
       component:HeroesComponent
+    },
+    {
+      path:'dashboard',
+      component:DashboardComponent
+    },
+    {
+      path:'',
+      redirectTo:'./dashboard',
+      pathMatch:'full'
     }
+    
   ]),
   ],
   providers: [HeroService],
