a7d84baff6fd61cba8ab1f69f686af75e61e72c2
asunwei <993525033@qq.com>
2017-08-11 18:31:00 +0800

    路由-4

--- myangular2/src/app/heroes.component.ts
+++ myangular2/src/app/heroes.component.ts
@@ -1,13 +1,13 @@
 import { Component,OnInit } from '@angular/core';
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
+import {Router} from '@angular/router'
 
 
 @Component({
   selector: 'my-heroes',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit{
     title = 'Tour of Heroes';
@@ -18,7 +18,7 @@ export class HeroesComponent implements OnInit{
     this.selectedHero = hero;
   }
     
-  constructor(private heroService:HeroService){}
+  constructor(private router:Router,private heroService:HeroService){}
 
   getHeroes():void{
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -28,6 +28,10 @@ export class HeroesComponent implements OnInit{
     this.getHeroes();
   }
   
+  gotoDetail():void{
+    this.router.navigate(['/detail',this.selectedHero.id]);
+  }
+
 }
 
 
