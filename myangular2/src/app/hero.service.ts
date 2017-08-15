d30f665bc822a624d0888be97e7cd409d55497fa
asunwei <18655519621@163.com>
2017-08-15 23:20:11 +0800

    HTTP-1.1

--- myangular2/src/app/hero.service.ts
+++ myangular2/src/app/hero.service.ts
@@ -1,18 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Hero} from './hero'
-import {HEROES} from './mock-heroes';
+import {Http} from '@angular/Http';
+import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 export class HeroService{
-    getHeroes():Promise<Hero[]>{
-        return Promise.resolve(HEROES);
-    }
-    
-    getHero(id:number):Promise<Hero>{
-      return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id ))
-    }
+  private heroesUrl = 'api/heroes';
 
-    getHeroesSlowly(): Promise<Hero[]> {
+  constructor(private http: Http){
+  }
+  getHeroes():Promise<Hero[]>{
+      return this.http.get(this.heroesUrl).
+      toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+    
+  private handleError(error : any) : Promise<any>{
+    console.error('An error occured',error);
+    return Promise.reject(error.message||error); 
+  }
+  getHero(id:number):Promise<Hero>{
+    return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id ))
+  }
+
+  getHeroesSlowly(): Promise<Hero[]> {
   return new Promise(resolve => {
     // Simulate server latency with 2 second delay
     setTimeout(() => resolve(this.getHeroes()), 20000);
