df178b2bc6fca4631c85101d35ea009007799eb1
asunwei <18655519621@163.com>
2017-08-20 22:20:58 +0800

    HTTP-1.3

--- myangular2/src/app/hero.service.ts
+++ myangular2/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Hero} from './hero'
-import {Http,Headers} from '@angular/Http';
+import {Http,Headers} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 
@@ -38,6 +38,21 @@ export class HeroService{
     return this.http.put(url,JSON.stringify(hero),{headers : this.headers})
     .toPromise()
     .then(() => hero)
+    .catch(this.handleError);
+  }
+  create(name:string): Promise<Hero>{
+    return this.http
+    .post(this.heroesUrl, JSON.stringify({name:name}),{headers:this.headers})
+    .toPromise()
+    .then(res =>res.json().data as Hero)
+    .catch(this.handleError);
+    
+  }
+  delete(id:number):Promise<void>{
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url,{headers:this.headers})
+    .toPromise()
+    .then(() => null) 
     .catch(this.handleError);
   }
 }   
